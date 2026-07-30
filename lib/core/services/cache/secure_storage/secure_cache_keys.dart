be84e945d5380bb3fd2b0905bc867a366969026e
MohamedIbrahemHamad <mohammedhamad.dev@gmail.com>
2026-07-30 08:15:42 +0300

    chore: implement Hive and SecureStorage services with key management

---
+++ lib/core/services/cache/secure_storage/secure_cache_keys.dart
@@ -0,0 +1,7 @@
+class SecureStorageKeys {
+  SecureStorageKeys._();
+
+  static const String refreshToken = 'refreshToken';
+  static const String accessToken = 'accessToken';
+  static const String role = 'role';
+}
