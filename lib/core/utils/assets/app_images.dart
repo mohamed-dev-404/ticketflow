bbccde1048c7facd1063251ee88a8d6ccc47864d
MohamedIbrahemHamad <mohammedhamad.dev@gmail.com>
2026-07-30 08:11:15 +0300

    chore: add asset files and utility classes for icons, images, and lotties, add analysis options for linting and formatting, and update pubspec.yaml with dependencies and assets

---
+++ lib/core/utils/assets/app_images.dart
@@ -0,0 +1,9 @@
+class AppImages {
+  AppImages._();
+
+  //* Logos
+  static const String mainLogo = 'assets/images/main_logo.png';
+  static const String drawerLogo = 'assets/images/drawer_logo.png';
+  static const String google = 'assets/images/google.png';
+
+}
