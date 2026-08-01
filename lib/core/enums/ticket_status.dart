95532962e4b9c90af20f412889ff40cf22d86a5f
MohamedIbrahemHamad <mohammedhamad.dev@gmail.com>
2026-08-01 22:15:45 +0300

    feat: add label, color, backgroundColor, and icon getters for TicketPriority and TicketStatus enums

--- lib/core/enums/ticket_status.dart
+++ lib/core/enums/ticket_status.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/material.dart';
 import 'package:hive_ce/hive.dart';
+import 'package:ticketflow/core/utils/colors/app_colors.dart';
 
 part 'ticket_status.g.dart';
 
@@ -9,5 +11,49 @@ enum TicketStatus {
   @HiveField(1)
   inProgress,
   @HiveField(2)
-  closed,
+  closed;
+
+  String get label {
+    switch (this) {
+      case TicketStatus.open:
+        return 'Open';
+      case TicketStatus.inProgress:
+        return 'In Progress';
+      case TicketStatus.closed:
+        return 'Closed';
+    }
+  }
+
+  Color get color {
+    switch (this) {
+      case TicketStatus.open:
+        return AppColors.statusOpen;
+      case TicketStatus.inProgress:
+        return AppColors.statusInProgress;
+      case TicketStatus.closed:
+        return AppColors.statusClosed;
+    }
+  }
+
+  Color get backgroundColor {
+    switch (this) {
+      case TicketStatus.open:
+        return AppColors.statusOpenBg;
+      case TicketStatus.inProgress:
+        return AppColors.statusInProgressBg;
+      case TicketStatus.closed:
+        return AppColors.statusClosedBg;
+    }
+  }
+
+  IconData get icon {
+    switch (this) {
+      case TicketStatus.open:
+        return Icons.radio_button_checked_rounded;
+      case TicketStatus.inProgress:
+        return Icons.hourglass_top_rounded;
+      case TicketStatus.closed:
+        return Icons.check_circle_rounded;
+    }
+  }
 }
