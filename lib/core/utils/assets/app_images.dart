1fc5fe498d9245ffe431c6021584e49f33a01c95
MohamedIbrahemHamad <mohammedhamad.dev@gmail.com>
2026-08-03 01:08:35 +0300

    refactor: update logo asset names in app_images.dart, add loading.json

--- lib/core/utils/assets/app_images.dart
+++ lib/core/utils/assets/app_images.dart
@@ -2,7 +2,7 @@ class AppImages {
   AppImages._();
 
   //* Logos
-  static const String mainLogo = 'assets/images/main_logo.png';
+  static const String iconLogo = 'assets/images/icon_logo.png';
   static const String drawerLogo = 'assets/images/drawer_logo.png';
   static const String google = 'assets/images/google.png';
 
