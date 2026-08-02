db5a403dd8ba654b38acde5791db373411af765a
MohamedIbrahemHamad <mohammedhamad.dev@gmail.com>
2026-08-03 00:52:48 +0300

    feat: refactor TicketCategory enum to improve label, color, backgroundColor, icon, and prefix getters

--- lib/core/enums/ticket_category.dart
+++ lib/core/enums/ticket_category.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/material.dart';
 import 'package:hive_ce/hive.dart';
+import 'package:ticketflow/core/utils/colors/app_colors.dart';
 
 part 'ticket_category.g.dart';
 
@@ -6,21 +8,12 @@ part 'ticket_category.g.dart';
 enum TicketCategory {
   @HiveField(0)
   technical,
+
   @HiveField(1)
   billing,
+
   @HiveField(2)
   general;
-
-  String get prefix {
-    switch (this) {
-      case TicketCategory.technical:
-        return 'TECH';
-      case TicketCategory.billing:
-        return 'BIL';
-      case TicketCategory.general:
-        return 'GEN';
-    }
-  }
 
   String get label {
     switch (this) {
@@ -30,6 +23,52 @@ enum TicketCategory {
         return 'Billing';
       case TicketCategory.general:
         return 'General';
+    }
+  }
+
+  Color get color {
+    switch (this) {
+      case TicketCategory.technical:
+        return AppColors.catTechnical;
+      case TicketCategory.billing:
+        return AppColors.catBilling;
+      case TicketCategory.general:
+        return AppColors.catGeneral;
+    }
+  }
+
+  Color get backgroundColor {
+    switch (this) {
+      case TicketCategory.technical:
+        return AppColors.catTechnical.withValues(alpha: .12);
+      case TicketCategory.billing:
+        return AppColors.catBilling.withValues(alpha: .12);
+      case TicketCategory.general:
+        return AppColors.catGeneral.withValues(alpha: .12);
+    }
+  }
+
+  IconData get icon {
+    switch (this) {
+      case TicketCategory.technical:
+        return Icons.code_rounded;
+
+      case TicketCategory.billing:
+        return Icons.receipt_long_rounded;
+
+      case TicketCategory.general:
+        return Icons.dashboard_customize_rounded;
+    }
+  }
+
+  String get prefix {
+    switch (this) {
+      case TicketCategory.technical:
+        return 'TECH';
+      case TicketCategory.billing:
+        return 'BIL';
+      case TicketCategory.general:
+        return 'GEN';
     }
   }
 }
