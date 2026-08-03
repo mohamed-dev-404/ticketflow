804a522841d798730b5e55585adc888c44662875
MohamedIbrahemHamad <mohammedhamad.dev@gmail.com>
2026-08-03 12:46:58 +0300

    feat: implement SearchCubit and SearchState for ticket search functionality

--- lib/features/search/presentation/view_models/search_cubit/search_state.dart
+++ lib/features/search/presentation/view_models/search_cubit/search_state.dart
@@ -1,1 +1,18 @@
-// TODO: Implement SearchState
+import 'package:flutter/foundation.dart';
+
+@immutable
+abstract class SearchState {}
+
+class SearchInitial extends SearchState {}
+
+class SearchLoading extends SearchState {}
+
+class SearchSuccess extends SearchState {}
+
+class SearchEmpty extends SearchState {}
+
+class SearchFailure extends SearchState {
+  final String errorMessage;
+
+  SearchFailure(this.errorMessage);
+}
